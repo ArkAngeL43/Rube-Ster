ab2a8bc4e76296c15552583dfad522fd3f3c6afa
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-08-19 20:12:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,14 @@ sudo gem install socket
 sudo gem install tty-spinner
 sudo apt-get install arp-scan 
 sudo apt-get install tshark 
+sudo apt-get install docker 
+sudo apt-get update 
+git clone  https://github.com/mackwic/colored.git
+cd colored 
+docker build -t colored_image .
+docker run --rm -it -v "$PWD":/src -u `id -u`:`id -g` colored_image /bin/bash -c "cargo build"
+echo 'testing'
+docker run --rm -it -v "$PWD":/src -u `id -u`:`id -g` colored_image /bin/bash -c "cargo test"
+cd port-scan && cd ps && ls && ps && rustc main.rs
+echo ' sucessfully built rust port-scanner using rustc '
+echo ' [!] exiting '
