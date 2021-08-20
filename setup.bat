b559d5cf1d646dd5b5cdc75904deffa53ba08430
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-08-20 19:48:00 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -3232,4 +3232,16 @@ pip install imdb
 pip install pip install IMDbPY
 pip install git+https://github.com/alberanid/imdbpy
 pip install imdbpy 
+sudo gem install colorize 
+sudo gem install socket
+sudo gem install tty-spinner
+git clone  https://github.com/mackwic/colored.git
+cd colored 
+docker build -t colored_image .
+docker run --rm -it -v "$PWD":/src -u `id -u`:`id -g` colored_image /bin/bash -c "cargo build"
+echo 'testing'
+docker run --rm -it -v "$PWD":/src -u `id -u`:`id -g` colored_image /bin/bash -c "cargo test"
+cd port-scan && cd ps && ls && ps && rustc main.rs
+echo ' sucessfully built rust port-scanner using rustc '
+echo ' [!] exiting '
 cls
