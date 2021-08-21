46f33530708006060a3952b840ced522fee2095b
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-08-20 23:14:13 -0400

    Update banner-ground.py

--- banner-ground.py
+++ banner-ground.py
@@ -7,12 +7,17 @@ from colorama import init
 import datetime as datetime 
 from datetime import datetime 
 
+init()
+
 def cs(X):
   t.sleep(X)
   if sys.platform == 'linux':
     os.system('clear')
     if sys.platform == 'win-32':
       os.system('cls')
+      if sys.platform == 'win-64':
+        os.system('cls')
+
 
 def oscheck():
         if sys.platform == 'linux':
@@ -33,11 +38,16 @@ def cls():
 def banner1():
   oscheck()
   print(Fore.YELLOW+" ")
-  print(Fore.YELLOW+"   .      .       _   _                 ___          ...            o             ___     #                 #")
-  print(Fore.YELLOW+"    ,,,,,      .  .:::.        '\\-//`    < - - - - -()_ \    o,*,(o o)        ` /_\ '         /_\ `*   #=ooO=========Ooo=#")
+  print(Fore.YELLOW+"    .      .      _   _          ___                  ...           o            ___            #                 #")
+  print(Fore.YELLOW+"    ,,,,,      .  .:::.          '\\-//`   < - - - - -()_ \     o,*,(o o)       ` /_\ '         /_\ `*   #=ooO=========Ooo=#")
   print(Fore.YELLOW+"   /(o o)\       :(o o):  .     (o o)             !!   | |   8(o o)(_)Ooo     - (o o) -       (o o)     #  \\  (o o)  //  #")
   print(Fore.YELLOW+"ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo------------------ooO-(_)---Ooo----ooO--(_)--Ooo-ooO--(_)--Ooo---------(_)-------- ")
 
-  cs(2)
-  oscheck()
-  banner1()
+if sys.platform == 'win32':
+    os.system('cls')
+if sys.platform == 'linux':
+    os.system('clear')
+
+cs(2)
+oscheck()
+banner1()
