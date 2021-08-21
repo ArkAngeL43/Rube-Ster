fc7ced15f816c940d1d6ed28988b9e955447b320
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-08-20 23:18:19 -0400

    Update readme.py

--- readme.py
+++ readme.py
@@ -4,6 +4,46 @@ import time as t
 from colorama import init 
 from colorama import Fore 
 
+init()
+
+def cs(X):
+  t.sleep(X)
+  if sys.platform == 'linux':
+    os.system('clear')
+    if sys.platform == 'win-32':
+      os.system('cls')
+      if sys.platform == 'win-64':
+        os.system('cls')
+
+
+def oscheck():
+        if sys.platform == 'linux':
+                print(" hello 1 ")
+                if sys.platform == 'win-32':
+                        def cs(X):
+                                t.sleep(X)
+                                os.system('cls')
+                                if sys.platform == 'win-64':
+                                        def cs(X):                                                
+                                                t.sleep(X)
+                                                os.system('cls')
+
+                                
+def cls():
+  oscheck()
+
+def banner1():
+  oscheck()
+  print(Fore.YELLOW+" ")
+  print(Fore.YELLOW+"    .      .      _   _          ___                  ...           o            ___            #                 #")
+  print(Fore.YELLOW+"    ,,,,,      .  .:::.          '\\-//`   < - - - - -()_ \     o,*,(o o)       ` /_\ '         /_\ `*   #=ooO=========Ooo=#")
+  print(Fore.YELLOW+"   /(o o)\       :(o o):  .     (o o)             !!   | |   8(o o)(_)Ooo     - (o o) -       (o o)     #  \\  (o o)  //  #")
+  print(Fore.YELLOW+"ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo------------------ooO-(_)---Ooo----ooO--(_)--Ooo-ooO--(_)--Ooo---------(_)-------- ")
+
+if sys.platform == 'win32':
+    os.system('cls')
+if sys.platform == 'linux':
+    os.system('clear')
 def clear():
     os.system(' clear ')
 
@@ -79,5 +119,9 @@ def banner():
 
     ''')
 banner()
+banner1()
 menue()
+
+#changes 
+# adsded system configuration for win, darwin, and linux 
 
